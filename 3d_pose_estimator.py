de7cf1ce531d0c1623892825ff9d6bfda817347b
Sanghyeon Kim <sanghyeon.kim98@gmail.com>
2024-03-25 18:50:58 +0900

    basic functionality complete

--- 3d_pose_estimator.py
+++ 3d_pose_estimator.py
@@ -24,44 +24,62 @@ import torch.nn as nn
 from lib.model.DSTformer import DSTformer
 from lib.utils.tools import get_config
 from lib.utils.learning import load_backbone
+import open3d as o3d
 
 try:
     from mmdet.apis import inference_detector, init_detector
+
     has_mmdet = True
 except (ImportError, ModuleNotFoundError):
     has_mmdet = False
 
-def halpe2h36m(joints, scores, vid_size):
-    x = np.concatenate([joints, scores[...,None]], axis=2)
-    kpts = np.zeros([1,17,3])
-    kpts[:,0,:] = x[:,19,:]
-    kpts[:,1,:] = x[:,12,:]
-    kpts[:,2,:] = x[:,14,:]
-    kpts[:,3,:] = x[:,16,:]
-    kpts[:,4,:] = x[:,11,:]
-    kpts[:,5,:] = x[:,13,:]
-    kpts[:,6,:] = x[:,15,:]
-    kpts[:,7,:] = (x[:,18,:] + x[:,19,:]) * 0.5
-    kpts[:,8,:] = x[:,18,:]
-    kpts[:,9,:] = x[:,0,:]
-    kpts[:,10,:] = x[:,17,:]
-    kpts[:,11,:] = x[:,5,:]
-    kpts[:,12,:] = x[:,7,:]
-    kpts[:,13,:] = x[:,9,:]
-    kpts[:,14,:] = x[:,6,:]
-    kpts[:,15,:] = x[:,8,:]
-    kpts[:,16,:] = x[:,10,:]
 
-    w, h = vid_size
-    scale = min(w,h) / 2.0
-    kpts[:,:,:2] = kpts[:,:,:2] - np.array([w, h]) / 2.0
-    kpts[:,:,:2] = kpts[:,:,:2] / scale
+def halpe2h36m(joints, scores, vid_size):
+
+    if joints.shape[0] != 1:
+        joints = joints[:1, :, :]
+        scores = scores[:1, :]
+
+    x = np.concatenate([joints, scores[..., None]], axis=2)
+    kpts = np.zeros([1, 17, 3])
+    kpts[:, 0, :] = x[:, 19, :]
+    kpts[:, 1, :] = x[:, 12, :]
+    kpts[:, 2, :] = x[:, 14, :]
+    kpts[:, 3, :] = x[:, 16, :]
+    kpts[:, 4, :] = x[:, 11, :]
+    kpts[:, 5, :] = x[:, 13, :]
+    kpts[:, 6, :] = x[:, 15, :]
+    kpts[:, 7, :] = (x[:, 18, :] + x[:, 19, :]) * 0.5
+    kpts[:, 8, :] = x[:, 18, :]
+    kpts[:, 9, :] = x[:, 0, :]
+    kpts[:, 10, :] = x[:, 17, :]
+    kpts[:, 11, :] = x[:, 5, :]
+    kpts[:, 12, :] = x[:, 7, :]
+    kpts[:, 13, :] = x[:, 9, :]
+    kpts[:, 14, :] = x[:, 6, :]
+    kpts[:, 15, :] = x[:, 8, :]
+    kpts[:, 16, :] = x[:, 10, :]
+
+    h, w = vid_size
+    scale = min(w, h) / 2.0
+    kpts[:, :, :2] = kpts[:, :, :2] - np.array([w, h]) / 2.0
+    kpts[:, :, :2] = kpts[:, :, :2] / scale
     motion = kpts
 
-    return torch.Tensor(motion[None,...])
+    return torch.Tensor(motion[None, ...])
+
 
 def process_one_image(
-    args, img, detector, pose2d_estimator, pose3d_estimator, visualizer=None, show_interval=0
+    args,
+    img,
+    detector,
+    pose2d_estimator,
+    pose3d_estimator,
+    visualizer2D=None,
+    visualizer3D=None,
+    points3D=None,
+    lineSet=None,
+    show_interval=0,
 ):
     """Visualize predicted keypoints (and heatmaps) of one image."""
 
@@ -85,12 +103,12 @@ def process_one_image(
 
     pose_prediction = data_samples.get("pred_instances", None)
 
-    coord_with_score = halpe2h36m(pose_prediction['keypoints'], pose_prediction['keypoint_scores'], img.shape[:2])
+    coord_with_score = halpe2h36m(
+        pose_prediction["keypoints"], pose_prediction["keypoint_scores"], img.shape[:2]
+    )
 
     predicted_3d_pos = pose3d_estimator(coord_with_score)
-    predicted_3d_pos[:,0,0,2]=0
-
-    print(predicted_3d_pos)
+    predicted_3d_pos[:, 0, 0, 2] = 0
 
     # show the results
     if isinstance(img, str):
@@ -98,8 +116,8 @@ def process_one_image(
     elif isinstance(img, np.ndarray):
         img = mmcv.bgr2rgb(img)
 
-    if visualizer is not None:
-        visualizer.add_datasample(
+    if visualizer2D is not None:
+        visualizer2D.add_datasample(
             "result",
             img,
             data_sample=data_samples,
@@ -112,6 +130,43 @@ def process_one_image(
             wait_time=show_interval,
             kpt_thr=args.kpt_thr,
         )
+
+    if visualizer3D is not None and points3D is not None and lineSet is not None:
+        points3D.points = o3d.utility.Vector3dVector(
+            predicted_3d_pos[0, 0].cpu().detach().numpy()
+        )
+        lineSet.points = points3D.points
+
+        lineSet.lines = o3d.utility.Vector2iVector(
+            np.asarray(
+                [
+                    [0, 1],
+                    [1, 2],
+                    [2, 3],
+                    [0, 4],
+                    [4, 5],
+                    [5, 6],
+                    [0, 7],
+                    [7, 8],
+                    [8, 10],
+                    [9, 10],
+                    [11, 8],
+                    [11, 12],
+                    [12, 13],
+                    [8, 14],
+                    [14, 15],
+                    [15, 16],
+                ]
+            )
+        )
+
+        vis_ctr = visualizer3D.get_view_control()
+        vis_ctr.set_up((0, -1, 0))
+
+        visualizer3D.update_geometry(points3D)
+        visualizer3D.update_geometry(lineSet)
+        visualizer3D.poll_events()
+        visualizer3D.update_renderer()
 
 
 def main():
@@ -186,10 +241,7 @@ def main():
         help="checkpoint to evaluate (file name)",
     )
     parser.add_argument(
-        "--camera-id",
-        type=int,
-        default=0,
-        help="for specifying the webcam ID"
+        "--camera-id", type=int, default=0, help="for specifying the webcam ID"
     )
 
     args = parser.parse_args()
@@ -206,7 +258,7 @@ def main():
         model_backbone = model_backbone.cuda()
 
     checkpoint = torch.load(args.evaluate, map_location=lambda storage, loc: storage)
-    model_backbone.load_state_dict(checkpoint['model_pos'], strict=True)
+    model_backbone.load_state_dict(checkpoint["model_pos"], strict=True)
     pose3d_estimator = model_backbone
     pose3d_estimator.eval()
 
@@ -226,12 +278,25 @@ def main():
     pose2d_estimator.cfg.visualizer.radius = args.radius
     pose2d_estimator.cfg.visualizer.alpha = args.alpha
     pose2d_estimator.cfg.visualizer.line_width = args.thickness
-    visualizer = VISUALIZERS.build(pose2d_estimator.cfg.visualizer)
+    visualizer2D = VISUALIZERS.build(pose2d_estimator.cfg.visualizer)
     # the dataset_meta is loaded from the checkpoint and
     # then pass to the model in init_pose_estimator
-    visualizer.set_dataset_meta(
+    visualizer2D.set_dataset_meta(
         pose2d_estimator.dataset_meta, skeleton_style=args.skeleton_style
     )
+
+    ## 3D visualization with Open3D
+    visualizer3D = o3d.visualization.Visualizer()
+    visualizer3D.create_window()
+
+    points3D = o3d.geometry.PointCloud()
+    coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=0.5, origin=[1, 0, 0]
+    )
+    lineSet = o3d.geometry.LineSet()
+    visualizer3D.add_geometry(points3D)
+    visualizer3D.add_geometry(coordinate_frame)
+    visualizer3D.add_geometry(lineSet)
 
     cap = cv2.VideoCapture(args.camera_id)
 
@@ -243,7 +308,16 @@ def main():
 
         # topdown pose estimation
         process_one_image(
-            args, frame, detector, pose2d_estimator, pose3d_estimator, visualizer, 0.001
+            args,
+            frame,
+            detector,
+            pose2d_estimator,
+            pose3d_estimator,
+            visualizer2D,
+            visualizer3D,
+            points3D,
+            lineSet,
+            0.001,
         )
 
         # press ESC to exit
@@ -251,6 +325,7 @@ def main():
             break
 
     cap.release()
+    visualizer3D.destroy_window()
 
 
 if __name__ == "__main__":
